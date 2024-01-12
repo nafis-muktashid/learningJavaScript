06d5fc360183e01593a1ce0322022074ca0157af
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-01-13 01:23:10 +0600

    Update script.js

--- 05-Underneth-the-JS/script.js
+++ 05-Underneth-the-JS/script.js
@@ -1,1 +1,18 @@
-"use script";
+'use strict';
+
+/*
+JavaScript - JavaSctipt is a high-level, garbage-collected, Interpreted or just-in-time compiled, 
+    multi-paradigm, prototype-based object oriented, first-class functioined, 
+    dynamically typed, single-threaded, non-blocking event looped programming language.
+
+Paradigm - An approach and mindset of structuring code, which will direct your 
+    coding style and technique.
+    
+    Types of paradigm
+    1. Procedural programming
+            linear normal coding.
+    2. Object-oriented programming (OOP)
+    3. Functional programming (FR)
+
+
+*/
