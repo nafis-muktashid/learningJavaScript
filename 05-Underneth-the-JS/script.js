5113ce84f0d80d155c53fb74dd9f6010d9ee0646
Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2024-01-21 01:58:17 +0600

    Update script.js

--- 05-Underneth-the-JS/script.js
+++ 05-Underneth-the-JS/script.js
@@ -66,3 +66,51 @@ calcAge(1991);
 
 // console.log(age);   //Won't work. Cause it is in a child function scope.
 // printAge();         //Similar reason for this too
+
+
+
+
+
+// Hoisting and TDZ in Practice
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Jonas";
+let job = "teacher";
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
